d3000480a0b955affddc56dd9c56a82a7165e59f
anton bergman <antonbergman@hotmail.com>
2023-10-04 11:34:48 +0200

    Add comment and writeLine in Program.cs

--- GitTEst/Program.cs
+++ GitTEst/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
+            //Denna kommentar finns bara i branchen Anton.
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Branch");
         }
     }
 }
